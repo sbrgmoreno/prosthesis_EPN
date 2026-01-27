d5034f99de2bf0fdd8d2c89983abd4bec9157a52
Mario Moreno Pallares <PImgmp@gmail.com>
2026-01-27 13:12:34 -0500

    rainbow-lite

--- agents/load_agent.m
+++ agents/load_agent.m
@@ -45,6 +45,8 @@ switch agent_id
         agent = agent_00_oldy(observationInfo, actionInfo);
    case "00_oldy_dueling"
         agent = agent_00_oldy_dueling(observationInfo, actionInfo);
+   case "00_rainbow_lite_dueling"
+        agent = agent_00_rainbow_lite_dueling(observationInfo, actionInfo);
 
     otherwise
         disp(param_name)
